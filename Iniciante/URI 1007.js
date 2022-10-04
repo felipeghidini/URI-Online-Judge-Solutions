20ffb7084d9732d348f8a06b2e2e3cba5af16774
felipeghidini <lipeghidini18@gmail.com>
2022-10-04 06:29:26 -0300

    1007 e 1008

---
+++ Iniciante/URI 1007.js
@@ -0,0 +1,30 @@
+/*DIFERENÇA
+Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+
+Entrada
+O arquivo de entrada contém 4 valores inteiros.
+
+Saída
+Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var a = parseFloat(lines.shift());
+var b = parseFloat(lines.shift());
+var c = parseFloat(lines.shift());
+var d = parseFloat(lines.shift());
+
+var diferenca = ((a * b) - (c * d));
+
+console.log('DIFERENCA = ' + diferenca);
+*/
+
+const a = 5;
+const b = 6;
+const c = 7;
+const d = 8;
+
+const diferenca = ((a * b) - (c * d));
+
+console.log('DIFERENCA = ' + diferenca);
---
+++ Iniciante/URI 1008.js
@@ -0,0 +1,29 @@
+/*SALÁRIO
+Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+
+Entrada
+O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o número, quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
+
+Saída
+Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $.
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var funcionarios = parseInt(lines.shift());
+var horas = parseInt(lines.shift());
+var valorPorHora = parseFloat(lines.shift());
+var salario = horas * valorPorHora;
+
+console.log("NUMBER = " + funcionarios);
+console.log("SALARY = U$ " + salario.toFixed(2));
+*/
+
+const number = 25;
+const horasTrabalhadas = 100;
+const valorPorHora = 5.50;
+
+const salary = horasTrabalhadas * valorPorHora;
+
+console.log('NUMBER = ' + number);
+console.log('SALARY U$ = ' + salary);
